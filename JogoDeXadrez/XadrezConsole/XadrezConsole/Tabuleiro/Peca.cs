ed6027598a92bf4d5f70bc964c0c0a23d4689467
Joselito <junior-joselito@hotmail.com>
2021-10-25 15:29:56 -0300

    Comecando a implementar o tabuleiro

---
+++ JogoDeXadrez/XadrezConsole/XadrezConsole/Tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using XadrezConsole.Tabuleiro.Enums;
+
+
+namespace XadrezConsole.Tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicoes { get; set; }
+        public Cor Cores { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiros Tab { get; protected set; }
+
+        public Peca(Posicao posicoes, Cor cores, Tabuleiros tab)
+        {
+            Posicoes = posicoes;
+            Cores = cores;
+            Tab = tab;
+            QtdMovimentos = 0;
+        }
+
+    }
+}
---
+++ JogoDeXadrez/XadrezConsole/XadrezConsole/Tabuleiro/Tabuleiros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace XadrezConsole.Tabuleiro
+{
+    class Tabuleiros
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas { get; set; }
+
+        public Tabuleiros(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
+    }
+}
