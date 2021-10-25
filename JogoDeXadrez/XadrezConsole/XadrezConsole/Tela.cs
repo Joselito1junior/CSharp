5c28f61aa6891e22fb0e470cccdd3a71ee727432
Joselito <junior-joselito@hotmail.com>
2021-10-25 17:20:42 -0300

    Melhorando a implementacao do metodo imprimir tabuleiro

--- JogoDeXadrez/XadrezConsole/XadrezConsole/Tela.cs
+++ JogoDeXadrez/XadrezConsole/XadrezConsole/Tela.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using XadrezConsole.Tabuleiro;
+using XadrezConsole.Tabuleiro.Enums;
 
 namespace XadrezConsole
 {
@@ -10,15 +11,34 @@ namespace XadrezConsole
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for(int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.RetornaPeca(i, j) == null)
                         Console.Write("* ");
-                    else                         
-                        Console.Write(tab.RetornaPeca(i, j) + " ");
+                    else
+                    {
+                        ImprimirPeca(tab.RetornaPeca(i, j));
+                        Console.Write(" ");
+                    }                         
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cores == Cor.Branca)
+                Console.Write(peca);
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+                
         }
     }
 }
