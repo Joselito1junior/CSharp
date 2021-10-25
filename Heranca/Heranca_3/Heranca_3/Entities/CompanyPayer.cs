0287f017d30de656009bb0c82f1c610004fa05fe
Joselito <junior-joselito@hotmail.com>
2021-10-25 10:20:10 -0300

    Heranca e polimorfismo

---
+++ Heranca/Heranca_3/Heranca_3/Entities/CompanyPayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Heranca_3.Entities
+{
+    class CompanyPayer : TaxPayer
+    {
+        public int NumberEmployees { get; set; }
+
+        public CompanyPayer()
+        {
+
+        }
+        public CompanyPayer(string name, double anualIncome, int numberEmployees): base(name, anualIncome)
+        {
+            NumberEmployees = numberEmployees;
+        }
+        public override double TaxPaid()
+        {
+            return (NumberEmployees < 10) ? AnualIncome * 0.16 : AnualIncome * 0.14;
+        }
+    }
+}
---
+++ Heranca/Heranca_3/Heranca_3/Entities/IndividualPayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Heranca_3.Entities
+{
+    class IndividualPayer : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public IndividualPayer()
+        {
+
+        }
+
+        public IndividualPayer(string name, double anualIncome, double healthExpenditures) :base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double TaxPaid()
+        {
+            if (AnualIncome < 20000)
+            {
+                return AnualIncome * 0.15;
+            }
+            else
+            {
+                return (AnualIncome * 0.25) - (HealthExpenditures * 0.5);
+            }
+        }
+    }
+}
