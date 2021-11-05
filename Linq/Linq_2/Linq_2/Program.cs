8baa71bc9953e84a934c77692dd9bf9e4cbf74ca
Joselito <junior-joselito@hotmail.com>
2021-11-05 17:34:25 -0300

    Código com a implementacao alternativa sql

--- Linq/Linq_2/Linq_2/Program.cs
+++ Linq/Linq_2/Linq_2/Program.cs
@@ -37,27 +37,35 @@ namespace Linq_2
                 new Product() {Id = 11, Name = "Level", Price = 70.00, Category = c1} 
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
+
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.00);
+            var r1 = from p in products where p.Category.Tier == 1 && p.Price < 900 select p;
             Print("TIER 1 AND PRICE < 900: ", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            // var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 = from p in products where p.Category.Name == "Tools" select p.Name;
             Print("PRRODUCTS FROM TOOLS: ", r2);
 
             //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => p.Name + ", " + p.Price + ", " + p.Category.Name);
             //Ou
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 = from p in products where p.Name[0] == 'C' select new { p.Name, p.Price, CategoryName = p.Category.Name };
             Print("PRRODUCTS FROM TOOLS THAT BEGIN WITH C: ", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 = from p in products where p.Category.Tier == 1 orderby p.Name orderby p.Price select p;
             Print("TIER 1 ORDER BY PRICE THEN BY NAME: ", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 = (from p in r4 select p).Skip(2).Take(4);
             Print("TIER 1 ORDER BY PRICE THEN BY NAME SKUP 2 TAKE 4", r5);
 
-            var r6 = products.FirstOrDefault();
+            //var r6 = products.FirstOrDefault();
+            var r6 = (from p in products select p).FirstOrDefault();
             Console.WriteLine("FIRST TEST1 or DEFAULT: " + r6);
 
-            var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            //var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
+            var r7 = (from p in products where p.Price > 3000 select p).FirstOrDefault();
             Console.WriteLine("FIRST TEST2 or DEFAULT: " + r7);
 
             var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
@@ -84,7 +92,8 @@ namespace Linq_2
             var r15 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
             Console.WriteLine("CATEGORY 5 AGGREGATE SUM: " + r15);
 
-            var r16 = products.GroupBy(p => p.Category);
+            //var r16 = products.GroupBy(p => p.Category);
+            var r16 = from p in products group p by p.Category;
             foreach(IGrouping<Category, Product> group in r16)
             {
                 Console.WriteLine("Category " + group.Key.Name + ": ");
