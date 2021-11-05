0a50786d498ddc49187cbba8a1e3d774bd9e9984
Joselito <junior-joselito@hotmail.com>
2021-11-05 12:23:28 -0300

    Exemplos de Delegates

---
+++ Delegates/Delegates_1/Delegates_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Delegates_1.Services;
+
+namespace Delegates_1
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        delegate void BinaryNumericOperationMulticast(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            double result = op(a, b);
+            double result_1 = op.Invoke(a, b);
+            Console.WriteLine(result);
+            Console.WriteLine(result_1);
+
+            //Multicast
+            BinaryNumericOperationMulticast op1 = CalculationService.ShowSum;
+            op1 += CalculationService.ShowMax;
+            op1(a, b);
+        }
+    }
+}
