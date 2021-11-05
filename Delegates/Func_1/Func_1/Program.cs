9dadb8ad451f92c8503d4224033f94c52c278614
Joselito <junior-joselito@hotmail.com>
2021-11-05 15:08:37 -0300

    Teste inicial com Linq

--- Delegates/Func_1/Func_1/Program.cs
+++ Delegates/Func_1/Func_1/Program.cs
@@ -16,27 +16,27 @@ namespace Func_1
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            //List<string> result = list.Select(NameUpper).ToList();
-            //foreach (string s in result)
-            //{
-            //    Console.WriteLine(s);
-            //}
+            List<string> result = list.Select(NameUpper).ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
 
-            ////Outra forma
-            //Func<Product, string> func = NameUpper;
-            //List<string> result_1 = list.Select(func).ToList();
-            //foreach (string s in result_1)
-            //{
-            //    Console.WriteLine(s);
-            //}
+            //Outra forma
+            Func<Product, string> func = NameUpper;
+            List<string> result_1 = list.Select(func).ToList();
+            foreach (string s in result_1)
+            {
+                Console.WriteLine(s);
+            }
 
-            ////Outra forma (Não foi colocado chaves pois essa função retorna um valor)
-            //Func<Product, string> func_1 = p => p.Name.ToUpper();
-            //List<string> result_2 = list.Select(func_1).ToList();
-            //foreach (string s in result_2)
-            //{
-            //    Console.WriteLine(s);
-            //}
+            //Outra forma (Não foi colocado chaves pois essa função retorna um valor)
+            Func<Product, string> func_1 = p => p.Name.ToUpper();
+            List<string> result_2 = list.Select(func_1).ToList();
+            foreach (string s in result_2)
+            {
+                Console.WriteLine(s);
+            }
 
             //Outra forma
             List<string> result_3 = list.Select(p => p.Name.ToUpper()).ToList();
@@ -44,7 +44,6 @@ namespace Func_1
             {
                 Console.WriteLine(s);
             }
-
         }
 
         static string NameUpper(Product p)
---
+++ Linq/Linq_1/Linq_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
