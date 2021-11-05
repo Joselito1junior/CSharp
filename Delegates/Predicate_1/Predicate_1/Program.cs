1d2b09f5142fa50e18ad9ffc4bc11a2e71b207fd
Joselito <junior-joselito@hotmail.com>
2021-11-05 12:34:15 -0300

    Exemple with Predicate

---
+++ Delegates/Predicate_1/Predicate_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Predicate_1.Entities;
+
+namespace Predicate_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProdutTest);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        public static bool ProdutTest(Product p)
+        {
+            return p.Price >= 100.00;
+        }
+    }
+}
