a3f02cc8e3bb45812c7010f05f63a95a2b148d82
Joselito <junior-joselito@hotmail.com>
2021-11-09 11:03:10 -0300

    Exemplos com linq

---
+++ Linq/Linq_3/Linq_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using Linq_3.Entities;
+
+namespace Linq_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            Console.Write("Digite o path: ");
+            string path = Console.ReadLine();
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(",");
+                    string name = line[0];
+                    double price = double.Parse(line[1], CultureInfo.InvariantCulture);
+
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            double r1 = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price: " + r1.ToString("F2"));
+
+            var names = list.Where(p => p.Price < r1).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach(string name in names)
+                Console.WriteLine(name);
+        }
+    }
+}
---
+++ Linq/Linq_4/Linq_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Linq_4.Entities;
+
+namespace Linq_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> emp = new List<Employee>();
+
+            Console.Write("Digite o Path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter the Salary: ");
+            double salValue = double.Parse(Console.ReadLine());
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while(!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(",");
+                    string name = line[0];
+                    string email = line[1];
+                    double salary = double.Parse(line[2], CultureInfo.InvariantCulture);
+
+                    emp.Add(new Employee(name, email, salary));
+                }
+            }
+
+            var emails = emp.Where(e => e.Salary > salValue).OrderBy(e => e.Email).Select(e => e.Email);
+
+            foreach(string email in emails)
+                Console.WriteLine(email);
+
+            var sum = emp.Where(e => e.Name[0] == 'M').Sum(e => e.Salary);
+            Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
