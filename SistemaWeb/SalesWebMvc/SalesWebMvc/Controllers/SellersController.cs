5e4e7a4fc501e99930b433a238ef995c531e10f2
Joselito <junior-joselito@hotmail.com>
2021-11-16 16:19:00 -0300

    SellerFormViewModel and Department select component

--- SistemaWeb/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ SistemaWeb/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartamentService _departamentService;
         
-        public SellersController (SellerService sellerService)
+        public SellersController (SellerService sellerService, DepartamentService departamentService)
         {
             _sellerService = sellerService;
+            _departamentService = departamentService;
         }
 
         public IActionResult Index()
@@ -21,14 +25,16 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departaments = _departamentService.FindAll();
+            var viewModel = new SellerFormViewModel{ Departaments = departaments };
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller sellser)
+        public IActionResult Create(Seller seller)
         {
-            _sellerService.Insert(sellser);
+            _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
     }
