6f6c478658f56b22413cc5ef51ada58fd3753723
Joselito <junior-joselito@hotmail.com>
2021-11-18 14:22:38 -0300

    Validation

--- SistemaWeb/SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ SistemaWeb/SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -9,18 +9,28 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size be between {2} and {1} characters")]
         public string Name { get; set; }
 
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage =" Enter a valid e-mail")]
         public string Email { get; set; }
 
-        [Display(Name = "Birth Date")]
+
         [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
+        [Required(ErrorMessage = "{0} required")]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Base Salary")]
+        [Required(ErrorMessage = "{0} required")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Range(100.0, 50000.0, ErrorMessage ="{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
         public Departament Departament { get; set; }
         public int DepartamentId { get; set; }
